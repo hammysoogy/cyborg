c7e7af8484eef26fc1b03267f12a355a78956de0
hammysoogy <bellaozuba@gmail.com>
2025-10-29 18:08:13 +0000

    Add InfJump module and remove AutoPearl functionality

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -4358,62 +4358,42 @@ run(function()
 end)
 	
 run(function()
-	local AutoPearl
-	local rayCheck = RaycastParams.new()
-	rayCheck.RespectCanCollide = true
-	local projectileRemote = {InvokeServer = function() end}
-	task.spawn(function()
-		projectileRemote = bedwars.Client:Get(remotes.FireProjectile).instance
-	end)
+	local InfJump
 	
-	local function firePearl(pos, spot, item)
-		switchItem(item.tool)
-		local meta = bedwars.ProjectileMeta.telepearl
-		local calc = prediction.SolveTrajectory(pos, meta.launchVelocity, meta.gravitationalAcceleration, spot, Vector3.zero, workspace.Gravity, 0, 0)
-	
-		if calc then
-			local dir = CFrame.lookAt(pos, calc).LookVector * meta.launchVelocity
-			bedwars.ProjectileController:createLocalProjectile(meta, 'telepearl', 'telepearl', pos, nil, dir, {drawDurationSeconds = 1})
-			projectileRemote:InvokeServer(item.tool, 'telepearl', 'telepearl', pos, pos, dir, httpService:GenerateGUID(true), {drawDurationSeconds = 1, shotId = httpService:GenerateGUID(false)}, workspace:GetServerTimeNow() - 0.045)
-		end
-	
-		if store.hand then
-			switchItem(store.hand.tool)
-		end
-	end
-	
-	AutoPearl = vape.Categories.Utility:CreateModule({
-		Name = 'AutoPearl',
+	InfJump = vape.Categories.Utility:CreateModule({
+		Name = 'InfJump',
 		Function = function(callback)
 			if callback then
-				local check
-				repeat
-					if entitylib.isAlive then
-						local root = entitylib.character.RootPart
-						local pearl = getItem('telepearl')
-						rayCheck.FilterDescendantsInstances = {lplr.Character, gameCamera, AntiFallPart}
-						rayCheck.CollisionGroup = root.CollisionGroup
-	
-						if pearl and root.Velocity.Y < -100 and not workspace:Raycast(root.Position, Vector3.new(0, -200, 0), rayCheck) then
-							if not check then
-								check = true
-								local ground = getNearGround(20)
-	
-								if ground then
-									firePearl(root.Position, ground, pearl)
-								end
-							end
-						else
-							check = false
-						end
+				_G.JumpHeight = 50
+
+				local function Action(Object, Function)
+					if Object ~= nil then
+						Function(Object)
 					end
-					task.wait(0.1)
-				until not AutoPearl.Enabled
+				end
+
+				InfJump.Connection = UIS.InputBegan:Connect(function(UserInput)
+					if UserInput.UserInputType == Enum.UserInputType.Keyboard and UserInput.KeyCode == Enum.KeyCode.Space then
+						Action(Player.Character and Player.Character:FindFirstChildOfClass("Humanoid"), function(self)
+							if self:GetState() == Enum.HumanoidStateType.Jumping or self:GetState() == Enum.HumanoidStateType.Freefall then
+								Action(self.Parent:FindFirstChild("HumanoidRootPart"), function(hrp)
+									hrp.Velocity = Vector3.new(0, _G.JumpHeight, 0)
+								end)
+							end
+						end)
+					end
+				end)
+			else
+				if InfJump.Connection then
+					InfJump.Connection:Disconnect()
+					InfJump.Connection = nil
+				end
 			end
 		end,
-		Tooltip = 'Automatically throws a pearl onto nearby ground after\nfalling a certain distance.'
+		Tooltip = 'Lets you infinitely jump in mid-air.'
 	})
 end)
+
 	
 run(function()
 	local AutoPlay
